6b8bf8627ec0f8a9eb681e20d17dde8ba5818218
Lucas Ribeiro <57465860+Memphyz@users.noreply.github.com>
2026-03-13 23:25:14 -0300

    feat: incluindo componente de homepage e icons

---
+++ src/app/features/homepage/homepage.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gb-homepage',
+  imports: [],
+  templateUrl: './homepage.html',
+  styleUrl: './homepage.scss',
+})
+export class Homepage {
+
+}
